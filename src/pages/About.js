5f0fc33a2ea379455e80459ceaf344be851fc8fa
naruenrtkhomut <naruenartkhomut@hotmail.com>
2024-09-09 06:00:14 +0700

    first commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -32,12 +32,24 @@ function About() {
                 </div>
                 <div class="about-paragraph">
                     <div class="about-get-to-know">Get to know about me</div>
-                    <article>
+                    <article class="about-article-left">
                         <div>
                             <img src="university-svgrepo-com.svg"/>
                         </div>
                         <div>
-                            <h2>Back in 2012 I was first year in Universtiy</h2>
+                            <h3>Back in 2012 I was first year in Universtiy</h3>
+                            <p>&emsp;&emsp;I got fleshman in university as Electronic and Telecommunication Engineering at Rajamangala University of Technology Isan Khonkaen Campus. I learned about electronic circuit and telecomunication. The first thing I knew about programming when I had class of Microcontroller and C Programming. But that time I got F grade because I didn't event know about anything in programming. First project of programming class is about calculator programming in window application by using C# winform I can't send in time and got F grade. After then I was learning a lot about programmin and I got spiration about IT department when I knew about hacking. In that time I was learning about system, OS and programming. That why I knew about system and programming.</p>
+                        </div>
+                    </article>
+                    <article class="about-article-right">
+                        <div>
+                            <h3>First job after graduated</h3>
+                            <p>&emsp;&emsp;I got first job as Failure Analysis Engineering at Delta Electronic Thailand PLC. I worked as Failure Analysis Engineer around 2.9 years. My main role is about reducing failure of product and improving productivity. In this product is about electronic charger of car for exporting. This job helped me to improve skill in analysis thinking and critical thinking when I have any problem and I have to solve it and made me to understand about electronic analysis. But I started to learn about programming when I got first job after left university. I lerned about C/C++ programming and made window application.</p>
+                            <h3>I left the job as Failure Analysis Engineer.</h3>
+                            <p>&emsp;&emsp;My second job is a Programmer at PPA Innovation. I decise to get a job as Programmer but I didn't graduate as Computer Science, Information Technology or Software Engineer. At first It's hard for me to get a job as Programmer I had many interview but can't pass the process. That time I got a job as Programmer at PPA Innovation. Because I passed to make winform in C# proramming.My main role is about create software as customer requirement for supporting main product. The main product is software in window applicaton. I had learned a lot about programming, database and website also.</p>
+                        </div>
+                        <div>
+                            <img src="work-case-svgrepo-com.svg"/>
                         </div>
                     </article>
                 </div>
